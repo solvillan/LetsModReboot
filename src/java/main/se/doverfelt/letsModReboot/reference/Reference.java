c0707d4190750dbde16eb4e93c32660bfa382e1c
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2014-06-27 20:20:15 +0200

    Added Reference

--- src/java/main/se/doverfelt/letsModReboot/reference/Reference.java
+++ src/java/main/se/doverfelt/letsModReboot/reference/Reference.java
@@ -1,26 +1,18 @@
-package se.doverfelt.letsModReboot;
-
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import se.doverfelt.letsModReboot.proxy.IProxy;
-import se.doverfelt.letsModReboot.reference.Reference;
+package se.doverfelt.letsModReboot.reference;
 
 /**
  * Copyright (c) 2014 Rickard Doverfelt
- *
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,29 +21,13 @@ import se.doverfelt.letsModReboot.reference.Reference;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+public final class Reference {
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
-public class LetsModReboot {
-
-    @Mod.Instance(Reference.MOD_ID)
-    public static LetsModReboot instance;
-
-    @SidedProxy(clientSide = Reference.CLINET_PROXY_NAME, serverSide = Reference.SERVER_PROXY_NAME)
-    public static IProxy proxy;
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
+    public static final String MOD_ID = "letsModReboot";
+    public static final String MOD_NAME = "Lets Mod Reboot";
+    public static final String VERSION = "@VERSION@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    public static final String SERVER_PROXY_NAME = "se.doverfelt.letsModReboot.proxy.ServerProxy";
+    public static final String CLINET_PROXY_NAME = "se.doverfelt.letsModReboot.proxy.ClientProxy";
 
 }
